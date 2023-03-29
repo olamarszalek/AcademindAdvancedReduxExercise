9875d41be3a4f2be73449ab25560f0d3b0ac1482
ola marszalek <ola.0608@gmail.com>
2023-03-29 21:19:14 +0200

    fetch useEffect and Notification component added

--- src/App.js
+++ src/App.js
@@ -3,10 +3,20 @@ import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import { useSelector } from 'react-redux';
 import { uiActions } from './store/ui-slice';
+import { useEffect } from 'react';
 
 function App() {
 
   const showCart = useSelector(state => state.ui.cartIsVisible);
+  const cart = useSelector(state => state.cart);
+
+  useEffect(() => {
+    fetch('https://react-backend-5806f-default-rtdb.europe-west1.firebasedatabase.app/cart.json', {
+      method: 'PUT', 
+      body: JSON.stringify(cart)
+    });
+  }, [cart]);
+
   return (
     <Layout>
       {showCart && <Cart />}
